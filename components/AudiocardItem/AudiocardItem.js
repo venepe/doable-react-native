6469e97030e1bd407f3d47bb650b49d096760b44
venepe <venepe@users.noreply.github.com>
2018-12-31 00:52:39 -0600

    Add active item opacity like roxie

--- components/AudiocardItem/AudiocardItem.js
+++ components/AudiocardItem/AudiocardItem.js
@@ -7,7 +7,9 @@ import {
 } from 'react-native';
 import gql from 'graphql-tag';
 import { propType } from 'graphql-anywhere';
+import { connect } from 'react-redux';
 import styles from './styles';
+import { getActiveAudiocard } from '../../reducers';
 
 class AudiocardItem extends Component {
   static propTypes = {
@@ -21,23 +23,41 @@ class AudiocardItem extends Component {
     onPress: () => {},
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+          activeAudiocard: nextProps.activeAudiocard,
+        }
+      }
+
   constructor(props) {
     super(props);
 
     this.state = {
       rowID: props.rowID,
       audiocardItem: props.audiocardItem,
+      activeAudiocard: props.activeAudiocard,
     };
+  }
+
+  componentDidUpdate(prevProps) {
+    const props = this.props;
+    if (props.activeAudiocard !== prevProps.activeAudiocard) {
+      this.setState({
+        activeAudiocard: props.activeAudiocard,
+      });
+    }
   }
 
   render() {
     let color = '#FF8A80';
 
     const audiocardItem = this.state.audiocardItem || {};
+    const activeAudiocard = this.state.activeAudiocard || {};
+    const opacity = audiocardItem.questionText === activeAudiocard.questionText ? .5 : 1.0;
 
     return (
         <TouchableOpacity onPress={() => this.props.onPress({ id: audiocardItem.id })}>
-          <View style={[styles.card, styles.container]}>
+          <View style={[styles.card, styles.container, { opacity }]}>
             <View style={styles.infoContainer}>
               <View style={styles.infoSubContainer}>
                 <Text style={[styles.title, { color }]}>{audiocardItem.questionText}</Text>
@@ -67,4 +87,10 @@ AudiocardItem.propTypes = {
   audiocardItem: propType(AudiocardItem.fragments.audiocardItem).isRequired,
 };
 
-export default AudiocardItem;
+const mapStateToProps = state => ({
+  activeAudiocard: getActiveAudiocard(state),
+});
+
+export default connect(
+  mapStateToProps,
+)(AudiocardItem);
