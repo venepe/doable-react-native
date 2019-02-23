2dc3c73104a49b13e5837078bab1b7c5c950212a
venepe <venepe@users.noreply.github.com>
2019-02-22 18:17:53 -0600

    Add persisant cache

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -37,10 +37,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@venepe/Doable", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40venepe%2FDoable%2F1.0.0%2F32a564de3d818d6e5ae28dbadd1387aa-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40venepe%2FDoable%2F1.0.0%2Fd33a36735be0cfd2dcaf098090194503-32.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
--- apolloClient.js
+++ apolloClient.js
@@ -1,7 +1,9 @@
 import { SecureStore } from 'expo';
+import { AsyncStorage } from 'react-native';
 import ApolloClient from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import { ApolloLink } from 'apollo-link';
 import { withClientState } from 'apollo-link-state';
 import { setContext } from 'apollo-link-context';
@@ -11,6 +13,10 @@ import { logonUser } from './helpers/logon';
 import Keys from './constants/Keys';
 
 const cache = new InMemoryCache();
+persistCache({
+  cache,
+  storage: AsyncStorage,
+});
 const httpLink = new HttpLink({ uri: GRAPHQL_URL });
 
 const stateLink = withClientState({
