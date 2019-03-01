cd7b91138cec55519dd882b114a18ce274b5b5a2
venepe <venepe@users.noreply.github.com>
2019-02-28 23:33:45 -0600

    Fix logout.  Remove session from auth0

--- helpers/logout.js
+++ helpers/logout.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { AuthSession, SecureStore } from 'expo';;
+import { SecureStore, WebBrowser } from 'expo';;
 import { Platform } from 'react-native';
 import { store } from '../App';
 import { setUID } from '../actions';
 import Keys from '../constants/Keys';
 const auth0ClientId = 'Z1nFXf7pX2wRyf5Ps4ArqYTyJ6fs5eE9';
 export const logoutUser = () => {
-  const returnUrl = AuthSession.getRedirectUrl();;
-  return fetch(`https://d0able.auth0.com/v2/logout?client_id=${auth0ClientId}&returnTo=${returnUrl}`)
+  const returnUrl = `com.venepe.doable://d0able.auth0.com/${Platform.OS}/com.venepe.doable/callback`;
+  return WebBrowser.openBrowserAsync(`https://d0able.auth0.com/v2/logout?client_id=${auth0ClientId}&returnTo=${returnUrl}`)
     .then((result) => {
       return SecureStore.setItemAsync(Keys.TOKEN_KEY, '')
     })
